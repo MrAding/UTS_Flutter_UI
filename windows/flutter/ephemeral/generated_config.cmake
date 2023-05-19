d3b6525fde47635967833e40024892c0b5064de1
Ahmad Fadhiil Muttaqin <46867774+MrAding@users.noreply.github.com>
2023-05-20 06:48:28 +0700

    uts_from_login

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\Semester 6\\Matkul Pemrograman Mobile\\UTS Semester 6\\UTS_Flutter_UI-main\\uts_from_login" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "PROJECT_DIR=D:\\Semester 6\\Matkul Pemrograman Mobile\\UTS Semester 6\\UTS_Flutter_UI-main\\uts_from_login"
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\Semester 6\\Matkul Pemrograman Mobile\\UTS Semester 6\\UTS_Flutter_UI-main\\uts_from_login\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\Semester 6\\Matkul Pemrograman Mobile\\UTS Semester 6\\UTS_Flutter_UI-main\\uts_from_login"
+  "FLUTTER_TARGET=D:\\Semester 6\\Matkul Pemrograman Mobile\\UTS Semester 6\\UTS_Flutter_UI-main\\uts_from_login\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9iNGZiMTEyMTRkZDJkZGE2Y2UwMTJkZDk4ZWE0OThlOWU4YjkxMjYyLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\Semester 6\\Matkul Pemrograman Mobile\\UTS Semester 6\\UTS_Flutter_UI-main\\uts_from_login\\.dart_tool\\package_config.json"
+)
